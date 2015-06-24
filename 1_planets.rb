dc351bbb19488155ec7e3a8a55ab050c357393de
rob_alef <ralef7@uchicago.edu>
2015-06-23 20:38:56 -0500

    question 1
    answered

--- 1_planets.rb
+++ 1_planets.rb
@@ -18,4 +18,6 @@ planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Saturn', 'Jupiter', 'Uranus', '
 # You may NOT, of course, use any hardcoded planet names in your code :-)
 #
 # Your code goes here:
-#
+planets.each_with_index do |planet, counter|
+	puts "Planet #{counter+1}: #{planet}"
+end
